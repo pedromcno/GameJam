0d49369098ec7e3fadda10f616c3c72dbc507dc3
Thomas Bonk <tbo@cybo.biz>
2014-07-12 14:21:05 +0200

    Fixed scaling again

--- src/js/boot.js
+++ src/js/boot.js
@@ -13,19 +13,7 @@
             this.game.input.maxPointers = 1;
             // this.game.stage.disableVisibilityChange = true;
 
-            if (this.game.device.desktop) {
-                this.game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-                this.game.stage.scale.pageAlignHorizontally = true;
-            } else {
-                this.game.stage.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-                this.game.stage.scale.minWidth = 480;
-                this.game.stage.scale.minHeight = 260;
-                this.game.stage.scale.maxWidth = 640;
-                this.game.stage.scale.maxHeight = 480;
-                this.game.stage.scale.forceLandscape = true;
-                this.game.stage.scale.pageAlignHorizontally = true;
-                this.game.stage.scale.setScreenSize(true);
-            }
+            this.game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
             this.game.state.start('preloader');
         }
     };

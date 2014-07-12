cae30d1554edb6fd5206640b2063b5ccccad4d2d
Thomas Bonk <tbo@cybo.biz>
2014-07-12 12:05:14 +0200

    Added background image

--- src/js/game.js
+++ src/js/game.js
@@ -1,47 +1,58 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  function Game() {
-    this.player = null;
-  }
-
-  Game.prototype = {
-
-    create: function () {
-      var x = this.game.width / 2
-        , y = this.game.height / 2;
-
-      this.player = this.add.sprite(x, y, 'player');
-      this.player.anchor.setTo(0.5, 0.5);
-      this.input.onDown.add(this.onInputDown, this);
-    },
-
-    update: function () {
-      var x, y, cx, cy, dx, dy, angle, scale;
-
-      x = this.input.position.x;
-      y = this.input.position.y;
-      cx = this.world.centerX;
-      cy = this.world.centerY;
-
-      angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
-      this.player.angle = angle;
-
-      dx = x - cx;
-      dy = y - cy;
-      scale = Math.sqrt(dx * dx + dy * dy) / 100;
-
-      this.player.scale.x = scale * 0.6;
-      this.player.scale.y = scale * 0.6;
-    },
-
-    onInputDown: function () {
-      this.game.state.start('menu');
+    function Game() {
+        this.player = null;
     }
 
-  };
+    Game.prototype = {
 
-  window['ganja-farmer'] = window['ganja-farmer'] || {};
-  window['ganja-farmer'].Game = Game;
+        create: function () {
+            this.addBackground();
+            this.addPlayer();
+            this.input.onDown.add(this.onInputDown, this);
+        },
+
+        addPlayer: function() {
+            var x = this.game.width / 2,
+                y = this.game.height / 2;
+
+            this.player = this.add.sprite(x, y, 'player');
+            this.player.anchor.setTo(0.5, 0.5);
+        },
+
+        addBackground: function() {
+            var background = this.add.sprite(0, 0, 'background');
+            background.scale.x = 2;
+            background.scale.y = 2;
+        },
+
+        update: function () {
+            var x, y, cx, cy, dx, dy, angle, scale;
+
+            x = this.input.position.x;
+            y = this.input.position.y;
+            cx = this.world.centerX;
+            cy = this.world.centerY;
+
+            angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
+            this.player.angle = angle;
+
+            dx = x - cx;
+            dy = y - cy;
+            scale = Math.sqrt(dx * dx + dy * dy) / 100;
+
+            this.player.scale.x = scale * 0.6;
+            this.player.scale.y = scale * 0.6;
+        },
+
+        onInputDown: function () {
+            this.game.state.start('menu');
+        }
+
+    };
+
+    window['ganja-farmer'] = window['ganja-farmer'] || {};
+    window['ganja-farmer'].Game = Game;
 
 }());
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -15,6 +15,7 @@
       this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
       this.load.setPreloadSprite(this.asset);
       this.load.image('player', 'assets/player.png');
+      this.load.image('background', 'assets/background.png');
       this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
     },
 
